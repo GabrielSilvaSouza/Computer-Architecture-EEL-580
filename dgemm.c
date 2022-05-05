c7b786c36b1ea6ada4ef07da630ba146884278f2
GabrielSilvaSouza <gabrieldasilvasouza21@gmail.com>
2022-05-05 20:28:06 -0300

    Polishing the code

--- dgemm.c
+++ dgemm.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
 
 
+void dgemm (int n, double *A, double *B, double *C) { 
+
+	for(int i =0; i < n; ++i) {
+		for(int j=0; j<n; ++j) {
+			double cij = C[i+j*n];
+			for (int k = 0; k < n; k++) {
+				cij += (A[i+k*n]) * (B[k+j*n]);
+			C[i+j*n] = cij;
+			}
+		}
+	}
+	
+	for(int i = 0; i < (n*n); i++) {
+		printf("%2.0f\n",C[i]);
+	}
+	
+	
+
+}
+
 int main (void) {
 
 
@@ -25,24 +45,7 @@ int main (void) {
     	};
 
 	
-	double *A = a;
-	double *B = b;
-	double *C = c;
-	
-	for(int i =0; i < n; ++i) {
-		for(int j=0; j<n; ++j) {
-			double cij = C[i+j*n];
-			for (int k = 0; k < n; k++) {
-				cij += (A[i+k*n]) * (B[k+j*n]);
-			C[i+j*n] = cij;
-			}
-		}
-	}
-	
-	for(int i = 0; i < 9; i++) {
-		printf("%2.0f",C[i]);
-	}
-	return 0;
+	dgemm(n,a,b,c);
 }
 
 
